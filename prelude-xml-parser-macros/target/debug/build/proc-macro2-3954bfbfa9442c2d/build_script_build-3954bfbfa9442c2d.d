aa8cd7f21ec99952cf4b53e97523c9f240e6aefc
Paul Sanders <paul@paulsanders.dev>
2024-06-24 12:25:09 -0400

    Add gitignore to macros

--- prelude-xml-parser-macros/target/debug/build/proc-macro2-3954bfbfa9442c2d/build_script_build-3954bfbfa9442c2d.d
+++
@@ -1,5 +0,0 @@
-/home/paul/development/rust/prelude-xml-parser/prelude-xml-parser-macros/target/debug/build/proc-macro2-3954bfbfa9442c2d/build_script_build-3954bfbfa9442c2d: /home/paul/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs
-
-/home/paul/development/rust/prelude-xml-parser/prelude-xml-parser-macros/target/debug/build/proc-macro2-3954bfbfa9442c2d/build_script_build-3954bfbfa9442c2d.d: /home/paul/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs
-
-/home/paul/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.86/build.rs:
--- prelude-xml-parser-macros/target/debug/deps/unicode_ident-ff7c658130c48170.d
+++
@@ -1,8 +0,0 @@
-/home/paul/development/rust/prelude-xml-parser/prelude-xml-parser-macros/target/debug/deps/libunicode_ident-ff7c658130c48170.rmeta: /home/paul/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/paul/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
-
-/home/paul/development/rust/prelude-xml-parser/prelude-xml-parser-macros/target/debug/deps/libunicode_ident-ff7c658130c48170.rlib: /home/paul/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/paul/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
-
-/home/paul/development/rust/prelude-xml-parser/prelude-xml-parser-macros/target/debug/deps/unicode_ident-ff7c658130c48170.d: /home/paul/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/paul/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
-
-/home/paul/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs:
-/home/paul/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs:
